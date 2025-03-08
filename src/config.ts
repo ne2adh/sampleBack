9b31e3c89c02900e1a62369722eb59a6f62c4835
Giancarlo Delgadillo <giancarlodcwork@gmail.com>
2025-03-07 20:42:03 -0400

    add conexion basededatos

--- src/config.ts
+++ src/config.ts
@@ -5,9 +5,10 @@ dotenv.config();
 
 export const pool = createPool({
   host: process.env.DB_HOST || "localhost",
+  port: Number(process.env.DB_PORT) || 5555,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASS || "",
-  database: process.env.DB_NAME || "agendadb",
+  database: process.env.DB_NAME || "agenda_db",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
